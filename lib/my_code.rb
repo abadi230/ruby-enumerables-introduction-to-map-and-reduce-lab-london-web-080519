857ad81aa550d1688f1d0781d31951af216972d8
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-08 19:35:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,8 +21,9 @@ def reduce_to_total(array, starting_point= 0)
 end
 
 def reduce_to_all_true(array)
+  # geven array : source_array = [1, 2, true, "razmatazz", false]
   array.reduce(0) {|string| 
-  string ? true : false}
+  string == true ? true : false}
 end
 
 def reduce_to_any_true(array)
